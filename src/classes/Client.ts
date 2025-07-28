78d6d4ce5fd1bc4a0e19e2638afbb3d86f9742ac
DrAwesome4333 <22409322+DrAwesome4333@users.noreply.github.com>
2025-07-28 11:02:36 -0600

    Removed POC 'hello'

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -74,7 +74,6 @@ export class Client {
         } else {
             this.options = { ...defaultClientOptions };
         }
-        console.log("hello");
         // Setup disconnection event handler to reset internal state.
         this.socket
             .on("disconnected", () => {
